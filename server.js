93fc993d2211693a58e47890d623e498006f003b
Nahid Ahmed <npahmed92@gmail.com>
2024-04-07 17:28:43 -0400

    add express and pool

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const express = require('express');
 const { Pool } = require('pg');
 
 // DATA ==============================================
+
 // APP/PORT ==========================================
+const app = express();
+const pool = new Pool({
+  user: 'postgres',
+  password: 'columbiadev',
+  host: 'localhost',
+  database: 'employeeManager_db',
+});
+
 // MIDDLEWARE ========================================
 // ROUTE =============================================
 // INITIALIZATION ====================================
