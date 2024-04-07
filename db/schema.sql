a7de8f950be4fc9e15c066cc82259711f2be97a6
Nahid Ahmed <npahmed92@gmail.com>
2024-04-07 03:16:44 -0400

    create employee table and implement foreign key

--- db/schema.sql
+++ db/schema.sql
@@ -15,3 +15,13 @@ CREATE TABLE role (
   department INTEGER,
   FOREIGN KEY (department) REFERENCES department(id)
 );
+
+CREATE TABLE employee (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INTEGER,
+  manager_id INTEGER,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id),
+);
