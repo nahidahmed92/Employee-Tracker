6ea2d3d2541fa313fd9a8bc24564dd28c2c37ed9
Nahid Ahmed <npahmed92@gmail.com>
2024-04-07 03:34:29 -0400

    insert data into roles table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,17 @@
-INSERT INTO department
+INSERT INTO department (name)
 VALUES
 ('Sales'),
 ('Engineering'),
 ('Finance'),
 ('Legal');
+
+INSERT INTO role (title, salary, department)
+VALUES
+('Sales Lead', 1, 100000),
+('Salesperson', 1, 80000),
+('Lead Engineer', 2, 150000),
+('Software Engineer', 2, 120000),
+('Account Manager', 3, 160000),
+('Accountant', 3, 125000),
+('Legal Team Lead', 4, 250000),
+('Lawyer', 4, 190000);
