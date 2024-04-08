21252f9e9a91ec107ad216493ed373b4a55cb3aa
Nahid Ahmed <npahmed92@gmail.com>
2024-04-07 20:43:30 -0400

    added NULL to manager_id and changed salary placement in roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
-INSERT INTO department (name)
+INSERT INTO departments (name)
 VALUES
 ('Sales'),
 ('Engineering'),
 ('Finance'),
 ('Legal');
 
-INSERT INTO role (title, salary, department)
+INSERT INTO roles (title, department, salary)
 VALUES
 ('Sales Lead', 1, 100000),
 ('Salesperson', 1, 80000),
@@ -16,13 +16,13 @@ VALUES
 ('Legal Team Lead', 4, 250000),
 ('Lawyer', 4, 190000);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-('John', 'Doe', 1),
+('John', 'Doe', 1, null),
 ('Mike', 'Chan', 2, 1),
-('Ashley', 'Rodriguez', 3),
+('Ashley', 'Rodriguez', 3, null),
 ('Kevin', 'Tupik', 4, 3),
-('Kunal', 'Singh', 5),
+('Kunal', 'Singh', 5, null),
 ('Malia', 'Brown', 6, 5),
-('Sarah', 'Lourd', 7),
-('Tom', 'Allen', 8, 7),
+('Sarah', 'Lourd', 7, null),
+('Tom', 'Allen', 8, 7);
