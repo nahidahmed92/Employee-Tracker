6507a113a455fd9f9fec9fb836f6a96b9570cd00
Nahid Ahmed <npahmed92@gmail.com>
2024-04-08 00:22:26 -0400

    add employee manager logo

---
+++ assets/asciiArt.js
@@ -0,0 +1,23 @@
+const employeeManagerLogo = console.log(`
+d88888b .88b  d88. d8888b. db       .d88b.  db    db d88888b d88888b
+88'     88'YbdP\`88 88  \`8D 88      .8P  Y8. \`8b  d8' 88'     88'
+88ooooo 88  88  88 88oodD' 88      88    88  \`8bd8'  88ooooo 88ooooo
+88~~~~~ 88  88  88 88~~~   88      88    88    88    88~~~~~ 88~~~~~
+88.     88  88  88 88      88booo. \`8b  d8'    88    88.     88.
+Y88888P YP  YP  YP 88      Y88888P  \`Y88P'     YP    Y88888P Y88888P
+
+
+    .88b  d88.  .d8b.  d8b   db  .d8b.   d888b  d88888b d8888b.
+    88'YbdP\`88 d8' \`8b 888o  88 d8' \`8b 88' Y8b 88'     88  \`8D
+    88  88  88 88ooo88 88V8o 88 88ooo88 88      88ooooo 88oobY'
+    88  88  88 88~~~88 88 V8o88 88~~~88 88  ooo 88~~~~~ 88\`8b
+    88  88  88 88   88 88  V888 88   88 88. ~8~ 88.     88 \`88.
+    YP  YP  YP YP   YP VP   V8P YP   YP  Y888P  Y88888P 88   YD
+
+
+`);
+
+module.exports = { employeeManagerLogo };
+
+const { employeeManagerLogo } = require('./assets/asciiArt.js');
+employeeManagerLogo;
