8314ba0737869db755f13d783fc988c26c7a4169
Nahid Ahmed <npahmed92@gmail.com>
2024-04-07 20:41:49 -0400

    update database name and add NOT NULL

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,27 @@
-DROP DATABASE IF EXISTS employeeManager_db;
-CREATE DATABASE employeeManager_db;
+DROP DATABASE IF EXISTS employee_manager_db;
+CREATE DATABASE employee_manager_db;
 
-c\ employeeManager_db;
+\c employee_manager_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(30)
+  name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
   id SERIAL PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department INTEGER,
-  FOREIGN KEY (department) REFERENCES department(id)
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department INTEGER NOT NULL,
+  FOREIGN KEY (department) REFERENCES departments(id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
   id SERIAL PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INTEGER,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
   manager_id INTEGER,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id),
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
